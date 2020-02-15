f1b8f7d9125bcfa9477e0bacdfaa2e3030c4dae8
DA0HN <vegancraft13@gmail.com>
2020-02-15 16:12:07 -0400

    Implementação afd

--- src/automato/Data.java
+++
@@ -1,8 +0,0 @@
-package automato;
-
-/*
- * @project automato
- * @author Gabriel on 15/02/2020
- */
-public class Data {
-}
--- src/automato/Main.java
+++ src/automato/Main.java
@@ -2,24 +2,34 @@ package automato;
 
 import automato.extract.ExtractListImpl;
 import automato.extract.ExtractMapImpl;
+import automato.extract.ExtractService;
 import automato.rules.Rules;
+import automato.rules.RulesListImpl;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String str  = "2, 1, 2, 1";
+        String str  = "2, 2, 1, 1";
         String str1 = "2, 2, 2";
         String str2 = "1, 1, 1, 1, 1, 1";
         String str3 = "2, 1, 1, 1, 1";
 
-        Rules rules = new ResourceReader(new ExtractMapImpl()).readAutomataRules("src/resources/input.txt", false);
+        ExtractService service = new ExtractMapImpl();
 
-        Automato auto = new Automato(rules, true);
-        boolean isAccepted = auto.evaluateExpression(str);
-        boolean isAccepted1 = auto.evaluateExpression(str);
-        boolean isAccepted2 = auto.evaluateExpression(str);
-        boolean isAccepted3 = auto.evaluateExpression(str);
+        Rules rules = new ResourceReader(service)
+                .readAutomataRules("src/resources/input.txt", false);
+
+        var automato = new Automato(rules, true);
+
+        boolean isAccepted = automato.evaluateExpression(str);
+        boolean isAccepted1 = automato.evaluateExpression(str);
+        boolean isAccepted2 = automato.evaluateExpression(str);
+        boolean isAccepted3 = automato.evaluateExpression(str);
 
         print(isAccepted, str);
         print(isAccepted1, str1);
